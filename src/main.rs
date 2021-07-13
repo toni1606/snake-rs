33bc5503c2597072c52f15de615ee881105224c3
Toni Guli <pllumiguli@icloud.com>
2021-07-13 14:00:39 +0200

    made Direction::opposite() public, finished implementing and testing Snake::new(), added use statements to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use snake::data_structures::point::Point;
+use snake::data_structures::snake::Snake;
+use snake::data_structures::direction::Direction;
 
 fn main() {
     println!("Hello, world!");
